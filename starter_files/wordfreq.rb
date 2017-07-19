eb0605591eeade623078b134b3a32bb72f2ec458
Geoff Humphreys <humphreys.geoff@gmail.com>
2017-07-19 16:09:04 -0400

    I have it splittig words and counting.  Need to finish test

---
+++ starter_files/wordfreq.rb
@@ -0,0 +1,45 @@
+require "pry"
+
+class Wordfreq
+  STOP_WORDS = ['a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from',
+    'has', 'he', 'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to',
+    'were', 'will', 'with']
+
+  def initialize(filename)
+    proclo = File.read(filename).downcase.gsub(/\W/, ' ')
+    words = proclo.split(/[^a-zA-Z]/) - STOP_WORDS
+    freqs = Hash.new(0)
+    words.each {|word| freqs[word] += 1}
+    freqs = freqs.sort_by {|x,y| y }
+    freqs.reverse!
+    puts freqs
+  end
+
+  def frequency(word)
+  end
+
+  def frequencies
+  end
+
+  def top_words(number)
+  end
+
+  def print_report
+
+  end
+end
+
+if __FILE__ == $0
+  filename = ARGV[0]
+  if filename
+    full_filename = File.absolute_path(filename)
+    if File.exists?(full_filename)
+      wf = Wordfreq.new(full_filename)
+      wf.print_report
+    else
+      puts "#{filename} does not exist!"
+    end
+  else
+    puts "Please give a filename as an argument."
+  end
+end
